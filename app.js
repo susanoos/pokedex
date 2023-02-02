3c67708b2e6631096f4e8ca6baa26c093b85725b
David <escxbar@gmail.com>
2023-02-01 18:16:11 -0800

    added features

--- app.js
+++ app.js
@@ -186,3 +186,10 @@ btnSearch.addEventListener("click", (e) => {
 
   searchInput.value = "";
 });
+
+var s = "grunt";
+var bear = { sound: "roar" };
+function roar() {
+  console.log(this.sound);
+}
+roar.apply(bear);
